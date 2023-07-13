f1b3504358667a8f16b34f4cf7415948dda20e29
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-07-13 15:48:49 +0200

    Improved menu.js
    added new function

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,12 +1,15 @@
 (() => {
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    closeMenuBtn: document.querySelectorAll("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.forEach(btn => { 
+  btn.addEventListener("click", toggleMenu);
+  }) 
+  // refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
